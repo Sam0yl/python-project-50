38655bf48f93d95f070545472faa889901b153e1
Evgeny Samoylov <samoyl@gmail.com>
2023-05-16 16:33:26 +0200

    fix output in plain style

--- gendiff/formats/plain.py
+++ gendiff/formats/plain.py
@@ -16,23 +16,33 @@ def format_value(value):
     return "'" + str(value).lower() + "'"
 
 
-def generate_plain_format(diff_tree, path=str()):
-    plain_diff_str = ''
-    for key in diff_tree:
-        path_key = f'{path}.{key}' if path else key
-        if diff_tree[key].get('diff_status') == 'common_child':
-            plain_diff_str += generate_plain_format(diff_tree[key].get(
-                'common_child_diff_tree'), path_key)
-        elif diff_tree[key].get('diff_status') == 'removed':
-            plain_diff_str += REMOVED.format(file_path=path_key)
-        elif diff_tree[key].get('diff_status') == 'added':
-            added_value = format_value(diff_tree[key].get('added_value'))
-            plain_diff_str += ADDED.format(file_path=path_key,
-                                           value=added_value)
-        elif diff_tree[key].get('diff_status') == 'changed':
-            added_value = format_value(diff_tree[key].get('added_value'))
-            removed_value = format_value(diff_tree[key].get('removed_value'))
-            plain_diff_str += CHANGED.format(file_path=path_key,
-                                             old_value=removed_value,
-                                             new_value=added_value)
-    return plain_diff_str
+def remove_last_line_feed(string):
+    return string[:-1]
+
+
+def generate_plain_format(diff):  # noqa: C901
+
+    def build_plain_diff_str(diff_tree, path=str()):
+        plain_diff_str = ''
+        for key in diff_tree:
+            path_key = f'{path}.{key}' if path else key
+            if diff_tree[key].get('diff_status') == 'common_child':
+                plain_diff_str += build_plain_diff_str(diff_tree[key].get(
+                    'common_child_diff_tree'), path_key)
+            elif diff_tree[key].get('diff_status') == 'removed':
+                plain_diff_str += REMOVED.format(file_path=path_key)
+            elif diff_tree[key].get('diff_status') == 'added':
+                added_value = format_value(diff_tree[key].get('added_value'))
+                plain_diff_str += ADDED.format(file_path=path_key,
+                                               value=added_value)
+            elif diff_tree[key].get('diff_status') == 'changed':
+                added_value = format_value(
+                    diff_tree[key].get('added_value'))
+                removed_value = format_value(
+                    diff_tree[key].get('removed_value'))
+                plain_diff_str += CHANGED.format(file_path=path_key,
+                                                 old_value=removed_value,
+                                                 new_value=added_value)
+        return plain_diff_str
+    result = remove_last_line_feed(build_plain_diff_str(diff))
+    return result
